65587dcc4cc9e2fc6ac9e34f7b9b9fbd281b8831
Gabriel Seri <serigabriel@yahoo.com>
2019-09-28 23:24:06 +0000

    new chatbot

--- chat/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.conf.urls import url
-from chat.views import ChatterBotApiView, ChatterBotAppView
-urlpatterns = [
-    url(r'^$', ChatterBotAppView.as_view(), name='main'),
-    url(r'^api/chatterbot/', ChatterBotApiView.as_view(), name='chatterbot'),
-]
--- chat_bot_pan/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-import chat.views
-
-urlpatterns = [
-    path('', chat.views.home, name='home'),
-    path('chat/', include('chat.urls')),
-    path('admin/', admin.site.urls),
-]
